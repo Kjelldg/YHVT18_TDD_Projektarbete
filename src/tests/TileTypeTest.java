d87229af7a146d93b20a6f096ec3bc82a551cd6d
Kjell Denti <kjelldg@gmail.com>
2018-03-26 15:18:03 +0200

    Added assets.

--- src/tests/TileTypeTest.java
+++ src/tests/TileTypeTest.java
@@ -1,40 +1,39 @@
 package tests;
 
-import GUI.TileType;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.internal.runners.statements.Fail;
-import org.omg.CORBA.DynAnyPackage.Invalid;
-
 import java.awt.image.BufferedImage;
+
+import GUI.TileType;
 
 public class TileTypeTest {
 
-    TileType tileType;
-    @Before public void init() {
-        tileType = new TileType();
-    }
+	TileType tileType;
 
-    @Test
-    public void tileType_getImageBasedOnId() {
-        int id = 2;
-        try {
-            BufferedImage bi = tileType.getImage(id);
-            Assert.assertEquals(bi, tileType.player);
-        } catch (TileType.InvalidIdException e) {
-            e.printStackTrace();
-        }
-    }
+	@Before
+	public void init() {
+		tileType = new TileType();
+	}
 
-    @Test(expected = TileType.InvalidIdException.class)
-    public void tileType_invalidIdExceptionWhenGettingImage() {
-        int id = 10;
-        try {
-            BufferedImage bi = tileType.getImage(id);
-            Assert.assertEquals(bi, tileType.player);
-        } catch (TileType.InvalidIdException e) {
-            e.printStackTrace();
-        }
-    }
+	@Test
+	public void tileType_getImageBasedOnId() {
+		int id = 2;
+		try {
+			BufferedImage bi = tileType.getImage(id);
+			Assert.assertEquals(bi, tileType.player);
+		} catch (TileType.InvalidIdException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test(expected = TileType.InvalidIdException.class)
+	public void tileType_invalidIdExceptionWhenGettingImage() {
+		int id = 10;
+		try {
+			BufferedImage bi = tileType.getImage(id);
+			Assert.assertEquals(bi, tileType.player);
+		} catch (TileType.InvalidIdException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
