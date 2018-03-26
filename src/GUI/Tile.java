3f408dc6fb90586d758bcca6ac23267c54b2c6e4
Kjell Denti <kjelldg@gmail.com>
2018-03-26 15:22:26 +0200

    Merged GUI-development into master.

--- src/GUI/Tile.java
+++ src/GUI/Tile.java
@@ -4,26 +4,25 @@ import java.awt.image.BufferedImage;
 
 public class Tile {
 
-    private int x;
-    private int y;
-    private BufferedImage image;
+	private int x;
+	private int y;
+	private BufferedImage image;
 
-    public Tile(int x, int y, BufferedImage image) {
-        this.x = x;
-        this.y = y;
-        this.image = image;
-    }
+	public Tile(int x, int y, BufferedImage image) {
+		this.x = x;
+		this.y = y;
+		this.image = image;
+	}
 
-    public int getX() {
-        return x;
-    }
+	public int getX() {
+		return x;
+	}
 
-    public int getY() {
-        return y;
-    }
+	public int getY() {
+		return y;
+	}
 
-
-    public BufferedImage getImage() {
-        return image;
-    }
+	public BufferedImage getImage() {
+		return image;
+	}
 }
--- src/GUI/Window.java
+++ src/GUI/Window.java
@@ -1,35 +1,36 @@
 package GUI;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JFrame;
+
 public class Window extends JFrame {
 
-    private List<Tile> tileArray = new ArrayList();
-    private TileType tileType = new TileType();
+	private List<Tile> tileArray = new ArrayList();
+	private TileType tileType = new TileType();
 
-    public void paint(Graphics g) {
-        for(Tile tile : tileArray) {
-            g.drawImage(tile.getImage(), tile.getX(), tile.getY()+26, null);
-        }
-    }
+	public void paint(Graphics g) {
+		for (Tile tile : tileArray) {
+			g.drawImage(tile.getImage(), tile.getX(), tile.getY() + 26, null);
+		}
+	}
 
-    public Window() {
-        setLayout(new BorderLayout());
-        setSize(200, 200);
-        setTitle("Game");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
-        setResizable(false);
-    }
+	public Window() {
+		setLayout(new BorderLayout());
+		setSize(200, 200);
+		setTitle("Game");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setResizable(false);
+	}
 
-    public void update(int[][] gameboard) throws TileType.InvalidIdException {
-        tileArray = tileType.generateTiles(gameboard);
-        repaint();
-    }
+	public void update(int[][] gameboard) throws TileType.InvalidIdException {
+		tileArray = tileType.generateTiles(gameboard);
+		repaint();
+	}
+
 }
-
