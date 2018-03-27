87d261920dce1ffd2beb6ed68bdd614638796f3a
kjelldg <Kjelldg@gmail.com>
2018-03-28 00:14:18 +0200

    Added tests for UserInputTests

--- src/tests/UserInputTests.java
+++ src/tests/UserInputTests.java
@@ -1,26 +1,41 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.event.KeyEvent;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import gameplay.RunnableGame;
 import gameplay.UserInput;
 
 public class UserInputTests {
-	
+
 	public static UserInput userInputTest;
+	public static KeyEvent keyEvent;
+	public static RunnableGame runnableGame;
 
 	@BeforeEach
-	void Init()
-	{
+	void Init() {
 		userInputTest = new UserInput();
+		runnableGame = new RunnableGame();
 	}
-	
+
 	@Test
-	void checkForUserInput ()
-	{
-		userInputTest.keyPressed(null);
+	void checkForUserInput() {
+		// char keyEventTestChar = keyEvent.getKeyChar();
+		// userInputTest.keyPressed(keyEvent);
+
+		assertEquals("", runnableGame.userInput);
+
+	}
+
+	@Test
+	void startGameMethodTest() {
+		boolean gameStartedTrue = userInputTest.startGame("YES");
+
+		assertEquals(true, gameStartedTrue);
 	}
 
 }
