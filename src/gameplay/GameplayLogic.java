4d6d4b9798e2d7ac93ddc2468b53ac224beec88d
kjelldg <Kjelldg@gmail.com>
2018-03-28 17:41:17 +0200

    Added back tests to UserInputTest & GameplayLogic class.

---
+++ src/gameplay/GameplayLogic.java
@@ -0,0 +1,88 @@
+package gameplay;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class GameplayLogic {
+
+	public ArrayList<Integer> treasureOne = new ArrayList<Integer>();
+	public ArrayList<Integer> treasureTwo = new ArrayList<Integer>();
+	public ArrayList<Integer> treasureThree = new ArrayList<Integer>();
+
+	public boolean openDoor(int treasures) {
+
+		// If treasures is 10 or higher, you can enter.
+		// If treasures is below 10, you are denied.
+
+		if (treasures >= 10) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isActive(LocalTime now) {
+
+		// Create start and end-time for lasers.
+		// At 18:01 they'll be active.
+		// At 07:59 they'll still be active, but 07:59:01 they'll be turned off.
+		// Between 08:00-18:00 the museum is open and no lasers will be active.
+
+		LocalTime open = LocalTime.parse("18:01");
+		LocalTime closed = LocalTime.parse("07:59");
+
+		if (now.isAfter(open) || now.isBefore(closed)) {
+			return true;
+
+		} else if (now.equals(open) || now.equals(closed)) {
+			return true;
+
+		}
+		return false;
+	}
+
+	public int checkGold(ArrayList<Integer> one, ArrayList<Integer> two, ArrayList<Integer> three) {
+
+		// Count each array list that hold treasures.
+		// Calculate the value of picked up treasures.
+		// Return total amount of gold.
+
+		int gold = 0;
+		int countOne = 0, countTwo = 0, countThree = 0;
+
+		for (int i = 0; i <= one.size(); i++) {
+			countOne = i;
+
+			for (int j = 0; j <= two.size(); j++) {
+				countTwo = j;
+			}
+
+			for (int k = 0; k <= three.size(); k++) {
+				countThree = k;
+			}
+		}
+
+		countOne = countOne * 1;
+		countTwo = countTwo * 2;
+		countThree = countThree * 3;
+
+		gold = countOne + countTwo + countThree;
+
+		return gold;
+	}
+
+	public void checkTreasure(int treasure) {
+
+		if (treasure == 1) {
+			treasureOne.add(treasure);
+
+		} else if (treasure == 2) {
+			treasureTwo.add(treasure);
+
+		} else if (treasure == 3) {
+			treasureThree.add(treasure);
+
+		}
+
+	}
+
+}
--- src/tests/GameplayTest.java
+++ src/tests/GameplayTest.java
@@ -1,91 +1,93 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+
 import java.time.LocalTime;
 import java.util.ArrayList;
-import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import gameplay.GameplayLogic;
 
 class GameplayTest {
 
-    public static GameplayLogic game;
-    public static LocalTime now;
+	public static GameplayLogic game;
+	public static LocalTime now;
 
-    public static ArrayList<Integer> treasureOne = new ArrayList<Integer>();
-    public static ArrayList<Integer> treasureTwo = new ArrayList<Integer>();
-    public static ArrayList<Integer> treasureThree = new ArrayList<Integer>();
+	public static ArrayList<Integer> treasureOne = new ArrayList<Integer>();
+	public static ArrayList<Integer> treasureTwo = new ArrayList<Integer>();
+	public static ArrayList<Integer> treasureThree = new ArrayList<Integer>();
 
-    @BeforeAll
-    static void Init() {
-        game = new GameplayLogic();
-        now = LocalTime.now();
-    }
+	@BeforeAll
+	static void Init() {
+		game = new GameplayLogic();
+		now = LocalTime.now();
+	}
 
-    @Test
-    void testOpenDoor_checkEntryBasedOnTreasures() {
+	@Test
+	void testOpenDoor_checkEntryBasedOnTreasures() {
 
-        //Test with 10 or higher passes if expected is true.
-        //Test with 9 or lower passes if expected is false.
-        //Both fails if expected is set to the opposite.
+		// Test with 10 or higher passes if expected is true.
+		// Test with 9 or lower passes if expected is false.
+		// Both fails if expected is set to the opposite.
 
-        boolean testOne = game.openDoor(5);
-        boolean testTwo = game.openDoor(10);
+		boolean testOne = game.openDoor(5);
+		boolean testTwo = game.openDoor(10);
 
-        assertEquals(false, testOne);
-        assertEquals(true, testTwo);
+		assertEquals(false, testOne);
+		assertEquals(true, testTwo);
 
-    }
+	}
 
+	@Test
+	void testIsActive_checkIfCurrentTimeIsWithinActiveHours() {
 
-    @Test
-    void testIsActive_checkIfCurrentTimeIsWithinActiveHours() {
+		// Test one with a set time that is in range of active hours.
+		// Test two was done with local time outside of active hours.
 
-        //Test one with a set time that is in range of active hours.
-        //Test two was done with local time outside of active hours.
+		LocalTime timeOne = LocalTime.parse("20:08");
+		LocalTime timeTwo = LocalTime.parse("12:45");
 
-        LocalTime timeOne = LocalTime.parse("20:08");
-        LocalTime timeTwo = LocalTime.parse("12:45");
+		boolean testOne = game.isActive(timeOne);
+		boolean testTwo = game.isActive(timeTwo);
 
-        boolean testOne = game.isActive(timeOne);
-        boolean testTwo = game.isActive(timeTwo);
+		assertEquals(true, testOne);
+		assertEquals(false, testTwo);
+	}
 
-        assertEquals(true, testOne);
-        assertEquals(false, testTwo);
-    }
+	@Test
+	void testCheckGold_checkGoldBasedOnTreasures() {
 
-    @Test
-    void testCheckGold_checkGoldBasedOnTreasures() {
+		// Create array list for each type of treasure.
+		// Worth is 1, 2 and 3 gold.
+		// In this test case we have picked up:
+		// One 1g-treasure
+		// Three 2g-treasures
+		// Two 3g-treasures
 
-        //Create array list for each type of treasure.
-        //Worth is 1, 2 and 3 gold.
-        //In this test case we have picked up:
-        //One 1g-treasure
-        //Three 2g-treasures
-        //Two 3g-treasures
+		treasureOne.add(1);
+		treasureTwo.add(2);
+		treasureTwo.add(2);
+		treasureTwo.add(2);
+		treasureThree.add(3);
+		treasureThree.add(3);
 
-        treasureOne.add(1);
-        treasureTwo.add(2); treasureTwo.add(2); treasureTwo.add(2);
-        treasureThree.add(3); treasureThree.add(3);
+		int gold = game.checkGold(treasureOne, treasureTwo, treasureThree);
 
-        int gold = game.checkGold(treasureOne, treasureTwo, treasureThree);
+		assertEquals(13, gold);
+	}
 
-        assertEquals(13, gold);
-    }
+	@Test
+	void testCheckTreasure_validateTreasureAddToArray() {
 
-    @Test
-    void testCheckTreasure_validateTreasureAddToArray() {
+		// Validate treasure based on character.
+		// Add value to correct array list.
 
-        //Validate treasure based on character.
-        //Add value to correct array list.
+		game.checkTreasure(2);
+		int expected = game.treasureTwo.size();
 
-        game.checkTreasure(2);
-        int expected = game.treasureTwo.size();
-
-        assertEquals(1, expected);
-    }
-
+		assertEquals(1, expected);
+	}
 
 }
-
-
